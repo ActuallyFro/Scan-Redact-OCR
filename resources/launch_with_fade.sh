81a17ab3327936627df89068d79cccf7a360311a
actuallyfro <actuallyfro@github.com>
2025-05-06 23:27:49 -0500

    {resources/} PATHS!

--- resources/launch_with_fade.sh
+++ resources/launch_with_fade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the path to your logo file
-logo_path="~/PRISM/resources/PRISM-Logo.png"
+logo_path="/opt/PRISM/resources/PRISM-Logo.png"
 
 # Check if feh is installed
 if ! command -v feh &> /dev/null; then
@@ -23,5 +23,17 @@ if ps -p "$icon_pid" > /dev/null; then
   kill "$icon_pid"
 fi
 
+# Check for the presence of the virtual environment
+if [ ! -d "./scan-env" ]; then
+  echo "Virtual environment 'scan-env' not found. Creating it now..."
+  python3 -m venv scan-env
+  source ./scan-env/bin/activate
+  # pip install -r requirements.txt
+  pip install python-sane pillow pytesseract pdf2image reportlab
+else
+  echo "Virtual environment 'scan-env' found. Activating it..."
+  source ./scan-env/bin/activate
+fi
+
 # Activate the virtual environment and run your Python script in a new terminal
-gnome-terminal -- bash -c 'source ./scan-env/bin/activate && python3 ./PRISM.py; bash'
+gnome-terminal -- bash -c 'source ./scan-env/bin/activate && python3 /opt/PRISM.py; bash'
